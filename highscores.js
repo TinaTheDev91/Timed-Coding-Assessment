6a05d943abea5b8ad8af1ca988cefa5c05e5fd1c
TinaTheDev91 <122223572+TinaTheDev91@users.noreply.github.com>
2023-02-19 10:35:17 -0500

    pending high scores page

--- highscores.js
+++ highscores.js
@@ -2,10 +2,12 @@
 // localStorage.getItem
 
 
-var highscoreList = //something on htmnl
+var highscoreList = document.querySelector(".high-score-list") //something on html
 var highscores = Json.parse(localStorage.getItem('finalScore')) || []
 
 highscoreList.innterhmtl = 
 highscores.map(score => {
-    return `<li class = 'scoressss'> ${score.name} - ${score.score}</li>`
+    return `<li class = 'scores'> ${score.name} - ${score.score}</li>`
 }).join('')
+
+highscoreList.appendChild(highscores);
